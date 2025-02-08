eb1849fe426d457e7339f1b8264272a245ddb52c
harishankar1608 <way2harishankar@gmail.com>
2025-02-08 10:19:54 +0530

    Delete expense-tracker directory

--- expense-tracker/src/utils/validation.js
+++
@@ -1,3 +0,0 @@
-export const nameValidation = /([a-zA-Z0-9_\s]+)/;
-export const emailValidation = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i;
-export const passwordValidation = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).*/;
